3c90c8788b1fa5a64fe010a997b68e4edf1cef8b
Henri Nijborg <h.nijborg@linq5.nl>
2017-01-19 22:16:06 +0100

    Commit before testing

--- app/code/local/Dealer4dealer/Xcore/Model/Catalog/Product/Api/V2.php
+++ app/code/local/Dealer4dealer/Xcore/Model/Catalog/Product/Api/V2.php
@@ -20,6 +20,13 @@ class Dealer4dealer_Xcore_Model_Catalog_Product_Api_V2 extends Mage_Catalog_Mode
             $result['tax_rate'] = $taxRate;
         }
 
+        $product = Mage::getModel('catalog/product')->load($productId);
+
+        /** @var Dealer4dealer_Xcore_Model_Custom_Attribute $customAttribute */
+        foreach ($this->_getCustomAttributes($product) as $customAttribute) {
+            $result['xcore_custom_attributes'][] = $customAttribute->toArray();
+        }
+
         return $result;
     }
 
@@ -61,5 +68,37 @@ class Dealer4dealer_Xcore_Model_Catalog_Product_Api_V2 extends Mage_Catalog_Mode
         $taxRate = Mage::getSingleton('tax/calculation')->getRate($rateRequest);
 
         return $taxRate;
+    }
+
+    /**
+     * @param Mage_Catalog_Model_Product $product
+     * @return array
+     */
+    protected function _getCustomAttributes($product)
+    {
+        $mapping = Mage::helper('dealer4dealer_xcore')->getMappingData(Dealer4dealer_Xcore_Helper_Data::XPATH_PRODUCT_COLUMNS_MAPPING);
+
+        /** @var Dealer4dealer_Xcore_Model_Custom_Attribute $customAttributes */
+        $customAttributes = Mage::getModel('dealer4dealer_xcore/custom_attribute');
+        $response = [];
+
+        foreach ($mapping as $column) {
+            $response[] = $customAttributes->setData([
+                'key'       => $column['exact_key'],
+                'value'     => $product->getData($column['column'])
+            ]);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param Mage_Catalog_Model_Product
+     */
+    protected function dispatchEvents($product)
+    {
+        Mage::dispatchEvent('dealer4dealer_xcore_product_product_custom_attributes', array(
+            'product' => $product,
+        ));
     }
 }
